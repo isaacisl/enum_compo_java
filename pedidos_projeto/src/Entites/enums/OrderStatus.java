db4d066923e7264b075930779430687d9647360f
Isaac, dev. <97575461+isaacisl@users.noreply.github.com>
2023-05-25 15:39:37 -0300

    My First Commit

---
+++ pedidos_projeto/src/Entites/enums/OrderStatus.java
@@ -0,0 +1,10 @@
+package Entites.enums;
+
+public enum OrderStatus {
+
+	PENDING_PAYAMENT,
+	PROCESSING,
+	SHIPPED,
+	DELIVERED;
+	
+}
---
+++ pedidos_projeto/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author isaac
+ *
+ */
+module pedidos_projeto {
+}
